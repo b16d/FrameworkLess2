1cbd8a06cca70d372f39c44a80f8311780f496bb
HLetourneur <herve.letourneur@meritis.fr>
2024-03-10 15:29:54 +0100

    add start time

--- framework/src/main/java/com/meritis/meetup/frameworkless/HammerApplication.java
+++ framework/src/main/java/com/meritis/meetup/frameworkless/HammerApplication.java
@@ -3,9 +3,15 @@ package com.meritis.meetup.frameworkless;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class HammerApplication {
     public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
         SpringApplication.run(HammerApplication.class);
+        LocalDateTime fin = LocalDateTime.now();
+        System.out.println("Temps démarrage : " + Duration.between(now, fin).toMillis() + "ms");
     }
 }
--- lib/src/main/java/com/meritis/meetup/frameworkless/HammerApplication.java
+++ lib/src/main/java/com/meritis/meetup/frameworkless/HammerApplication.java
@@ -2,10 +2,16 @@ package com.meritis.meetup.frameworkless;
 
 import io.javalin.Javalin;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class HammerApplication {
     public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
         Javalin.create()
                .get("helloworld", ctx -> ctx.result("HelloWorld !").status(200))
                .start(8081);
+       LocalDateTime fin = LocalDateTime.now();
+       System.out.println("Temps démarrage : " + Duration.between(now, fin).toMillis() + "ms");
     }
 }
